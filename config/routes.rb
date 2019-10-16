513addb13a818d6b5d6157833e22d9d900e280e3
Sebastian Reyes <seabassreyes@gmail.com>
2019-10-16 10:18:48 -0700

    YEET them files into the repo my guy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  #view new and update need to show
+  get 'cities/index'
   root "main#index"
+  #resources :cities
+  get 'cities/view'  
+  get 'cities/new'
+  post 'cities/create'
+  #this maps to same, so need to specify to: controller#action
+  get 'cities/update', to: 'cities#update'
+  post 'cities/update', to: 'cities#add'
 end
